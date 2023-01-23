2da4de1413df38cce293b9397c1d348ef4a96eb6
bagabatgita <eespinozasullon@gmail.com>
2023-01-23 07:11:30 -0500

    Proyecto 1- React

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+import QuoteBox from "./components/QuoteBox";
+import db from "./db/quotes.json";
+
+function App() {
+  const arrayColors = [
+    "#00c6ab",
+    "#c8ad8d",
+    "#f86f6f",
+    "#6aa3b4",
+    "15ab92",
+    "#1e91ed",
+  ];
+
+  const getRandom = (arrayElements) => {
+    const quantityValues = arrayElements.length;
+    const randomIndex = Math.floor(Math.random() * quantityValues);
+    return arrayElements[randomIndex];
+  };
+  const [quote, setQuote] = useState(getRandom(db));
+
+  const [color, setColor] = useState(getRandom(arrayColors));
+
+  const newQuote = () => {
+    setQuote(getRandom(db));
+    setColor(getRandom(arrayColors));
+  };
+
+  return (
+    <div className="App" style={{ backgroundColor: color }}>
+      <QuoteBox quote={quote} newQuote={newQuote} color={color} />
+    </div>
+  );
+}
+
+export default App;
